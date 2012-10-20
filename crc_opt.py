4c08da845c4eeabf231e34f90781babc6005772f
Thomas Pircher <tehpeh@gmx.net>
2012-10-20 15:53:30 +0100

    v0.7.11

--- crc_opt.py
+++ crc_opt.py
@@ -48,7 +48,7 @@ class Options(object):
 
     # Program details
     ProgramName    = "pycrc"
-    Version        = "0.7.10"
+    Version        = "0.7.11"
     VersionStr     = "%s v%s" % (ProgramName, Version)
     WebAddress     = "http://www.tty1.net/pycrc/"
 
