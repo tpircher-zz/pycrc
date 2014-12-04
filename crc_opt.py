b94dbc36ca411dd377a999462c7e6b16909db3ba
Thomas Pircher <tehpeh@gmx.net>
2014-12-04 21:40:45 +0000

    Bumped version number up to 0.8.2.

--- crc_opt.py
+++ crc_opt.py
@@ -46,7 +46,7 @@ class Options(object):
 
     # Program details
     ProgramName    = "pycrc"
-    Version        = "0.8.1"
+    Version        = "0.8.2"
     VersionStr     = "%s v%s" % (ProgramName, Version)
     WebAddress     = "http://www.tty1.net/pycrc/"
 
